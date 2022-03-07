d2c128e7faff6a8e09568656f7cce25e3041d8b0
lajanzen <lara-janzen@hotmail.com>
2022-03-07 16:04:56 +0100

    Import Components into ApP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Title from "./Components/Title";
+import SearchField from "./Components/SearchField";
+import ItemsList from "./Components/ItemsList";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <Title />
+      <SearchField />
+      <ItemsList />
+    </>
+  );
 }
 
 export default App;
